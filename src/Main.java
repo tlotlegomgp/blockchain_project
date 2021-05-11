c6e9fce7489779cb8e0a763e23eb232ceaff696e
Tlotlego <tlotlegomgp@gmail.com>
2021-05-11 13:24:57 +0200

    Implemented the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
+    ArrayList<Block> blockchain = new ArrayList<>();
+
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("HELLO WORLD");
+        // write your code here
+        Blockchain blockChain = new Blockchain(3);
+
+        blockChain.addBlock(blockChain.newBlock(new String[]{"Apple = $120.000", "Google - $140,000"}));
+        blockChain.addBlock(blockChain.newBlock(new String[]{"Amazon = $120.000", "Facebook - $140,000"}));
+        blockChain.addBlock(blockChain.newBlock(new String[]{"Netflix = $120.000", "Microsoft - $140,000"}));
+
+        System.out.println("Is Blockchain valid?: " + blockChain.isBlockChainValid());
+
+        System.out.println(blockChain);
+
 
     }
 }
